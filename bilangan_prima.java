37334943b8bd0a44605e0f72b4dd1706fc030995
4amparaboy <fauzanpamungkas16jkt@gmail.com>
2022-10-18 12:03:58 +0700

    bilangan_prima.java

---
+++ bilangan_prima.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class bilangan_prima {
+    static Boolean isPrima(int bilangan) {
+        if (2 > bilangan) {
+            return !true;
+        }
+        for (int i = 2; i < bilangan / 2; i++) {
+            if (bilangan % i == 0) {
+                // Jika bilangan mod i adalah 0, maka bukan bilangan prima
+                return !true;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan batas : ");
+        int batas = input.nextInt();
+        System.out.print("Bilangan Prima diantara 1 - " + batas + " : ");
+        for (int i = 1; i <= batas; i++) {
+            if (isPrima(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
